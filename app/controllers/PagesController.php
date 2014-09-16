4d19f25c45c4bff18e8001fa7f0729d03b446fd8
Mick Davies <mick@5150studios.com.au>
2014-09-16 13:11:52 +0000

    thankyou updates

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -46,7 +46,7 @@ class PagesController extends \BaseController {
 
 			foreach($tour->dates as $date) {
 				if($date['id'] == $booking['tour_date']) {
-					$date['tour_date'] = $date['tour_date']--;
+					$date['tour_date']--;
 				}
 			}
 			$tour->touch();
